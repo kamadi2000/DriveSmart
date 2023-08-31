ecfdbf69cc49d4257209543b7d0e6afea9e1c099
kamadi2000 <kmaliyanage@gmail.com>
2023-08-31 19:14:13 +0530

    Adding Screens

--- src/navigation/rootNavigator.js
+++ src/navigation/rootNavigator.js
@@ -3,10 +3,38 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function DashBoardScreen() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>DashBoard Screen</Text>
+        </View>
+    );
+}
+function CourseScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Course Screen</Text>
+        </View>
+    );
+}
+function PremiumScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Premium Screen</Text>
+        </View>
+    );
+}
+function ContactUsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Contact Screen</Text>
+        </View>
+    );
+}
+function AboutUsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>AboutUs Screen</Text>
         </View>
     );
 }
@@ -26,23 +54,30 @@ function SignUpScreen() {
 }
 
 const Stack = createNativeStackNavigator();
+const getIsSignedIn = () => {
+    // custom logic
+    return true
+};
 
 function RouteNavigator() {
-    const isSignedIn = false
+    const isSignedIn = getIsSignedIn()
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                isSignedIn ? (
-                <>
-                    <Stack.Screen name="Home" component={HomeScreen} />
-                    
-                </>
+                {isSignedIn ? (
+                    <>
+                        <Stack.Screen name="DashBoard" component={DashBoardScreen} />
+                        <Stack.Screen name="Course" component={CourseScreen} />
+                        <Stack.Screen name="Premium" component={PremiumScreen} />
+                        <Stack.Screen name="Contact us" component={ContactUsScreen} />
+                        <Stack.Screen name="About us" component={AboutUsScreen} />
+                    </>
                 ) : (
-                <>
-                    <Stack.Screen name="SignIn" component={SignInScreen} />
-                    <Stack.Screen name="SignUp" component={SignUpScreen} />
-                </>
-                );
+                    <>
+                        <Stack.Screen name="SignIn" component={SignInScreen} />
+                        <Stack.Screen name="SignUp" component={SignUpScreen} />
+                    </>
+                )}
 
             </Stack.Navigator>
         </NavigationContainer>
