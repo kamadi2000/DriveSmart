865db925f6a8ed721dcf026f72cff870f61092b3
kamadi2000 <kmaliyanage@gmail.com>
2023-09-07 20:13:10 +0530

    Dependency changes

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import RouteNavigator from './src/navigation/rootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
-import { GluestackUIProvider, Text, Box, config } from "@gluestack-ui/themed"
 
 export default function App() {
   return (
-    <GluestackUIProvider config={config.theme}>
-      <StatusBar/>
-        <NavigationContainer>
-          <RouteNavigator />
-        </NavigationContainer>
-
+    
       
-    </GluestackUIProvider>
+      
+      <NavigationContainer>
+        <StatusBar/>
+        <RouteNavigator /> 
+        </NavigationContainer>
+    
 
     // <View style={styles.container}>
     //   {/* <Text>Open up App.js to start working on your app!</Text> */}
