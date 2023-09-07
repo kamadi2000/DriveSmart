cb237a79d3f0be93c5b934e7491eb7beca35ad32
kamadi2000 <kmaliyanage@gmail.com>
2023-09-08 00:44:56 +0530

    add app constants

---
+++ src/helpers/constants.js
@@ -0,0 +1,10 @@
+import {Dimensions} from 'react-native';
+
+export const HEIGHT = Dimensions.get('window').height;
+export const WIDTH = Dimensions.get('window').width;
+export const SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+export const PRIMARY_COLOR = '#B5E3EA'
+export const SECONDARY_COLOR = '#54849A'
+export const BACKGROUND_COLOR = '#EDF3FF'
+export const CARD_COLOR = '#FFFFFF'
