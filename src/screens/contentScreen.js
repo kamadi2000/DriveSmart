0ab27eac5ab91062475453f675dee20cf8187dda
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-11 02:07:52 +0530

    content-added

--- src/screens/contentScreen.js
+++ src/screens/contentScreen.js
@@ -1,9 +1,9 @@
-import { View, Text, ScrollView} from 'react-native'
+import { View, Text, ScrollView, StyleSheet} from 'react-native'
 import React, { useEffect } from 'react'
 import { useRoute } from '@react-navigation/native'
 import { BACKGROUND_COLOR } from '../utils/colors'
-
-
+import { Video, ResizeMode } from 'expo-av';
+import { WebView } from 'react-native-webview';
 const ContentScreen = () => {
     const route = useRoute()
     const {heading} = route.params
@@ -12,12 +12,82 @@ const ContentScreen = () => {
     })
   return (
     <View style={{flex:1, backgroundColor:BACKGROUND_COLOR}}>
-      <ScrollView>
-        <Text style={{fontFamily:'Poppins_SemiBold', fontSize:20, textAlign:'center'}} >{heading}</Text>
-        
-      </ScrollView>
+      <View style={{borderColor:'lightblue', borderWidth:1, margin:10,marginBottom:55, borderRadius:5}}>
+        <ScrollView>
+          <Text style={{fontFamily:'Poppins_SemiBold', fontSize:20, textAlign:'center'}} >{heading}</Text>
+          <Video
+          // ref={video}
+          style={styles.video}
+          source={{
+            uri:"https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+          }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          // onPlaybackStatusUpdate={status => setStatus(() => status)}
+        />
+        <View style={{flex:1, margin:5}}>
+          <ScrollView>
+          <WebView
+      style={{flex: 1, height: 400, width: '100%', backgroundColor: 'transparent' }}
+      originWhitelist={['*']}
+      source={{
+        html: `
+          <div style='font-size:45px'>
+            <h4 style='text-align: center;'>Course Overview: Comprehensive Driving Test Exam Preparation for Sri Lanka</h4>
+            <p style='text-align:justify'>Welcome to our all-inclusive online course designed
+              to prepare you thoroughly for the driving test in Sri
+              Lanka. Whether you're a new driver or seeking to
+              refresh your knowledge, this course covers all the
+              necessary aspects of driving, legal guidelines, road
+              signs, practice exams, and interactive quizzes to ensure
+              your success on the road.</p>
+            <h4>Course Benefits:</h4>
+            <p>By completing this course, you will:
+              <ul>
+                <li>Develop a comprehensive grasp of Sri Lanka's driving laws and regulations.</li>
+                <li>Recognize and respond effectively to various road signs and signals.</li>
+                <li>Approach the driving test with confidence, prepared for both practical and theoretical segments.</li>
+                <li>Foster the confidence to handle different driving scenarios safely and efficiently.</li>
+              </ul>
+            </p>
+
+            <p style='text-align:justify'>
+              Join us on this journey
+              to becoming a skilled and informed driver on Sri Lanka's
+              roads. Together, we strive for safer and more responsible
+              driving practices that benefit everyone. <br />
+
+              Please bear in mind that this course is designed for
+              educational purposes only and does not replace official
+              driving education or guidance.<br />
+
+              Feel free to personalize this course overview to align with your
+              course content and objectives. Best of luck with developing your
+              course!<br />
+            </p>
+            <h4 style='text-align:center'>Thank You!</h4>
+          </div>
+        `,
+      }}
+    />
+    </ScrollView>
+        </View>
+        </ScrollView>
+      </View>
     </View>
   )
 }
 
 export default ContentScreen
+
+const styles = StyleSheet.create({
+  video: {
+    alignSelf: 'center',
+    width: 350,
+    height:200,
+    borderColor:'gray',
+    borderRadius:15,
+    borderWidth:1,
+  },
+});
--- src/screens/examScreen.js
+++ src/screens/examScreen.js
@@ -3,6 +3,8 @@ import React, { useState, useMemo } from 'react'
 import { BACKGROUND_COLOR } from '../utils/colors'
 import RadioGroup, {RadioButtonProps} from 'react-native-radio-buttons-group';
 import PrimaryButton from '../components/basic/PrimaryButton';
+import { WIDTH } from '../utils/constants';
+import { ScrollView } from 'react-native';
 
 const ExamScreen = () => {
     const [selectedId, setSelectedId] = useState();
@@ -30,72 +32,48 @@ const ExamScreen = () => {
     ]), []);
   return (
     <View style={{flex:1,backgroundColor:BACKGROUND_COLOR}}>
+        <ScrollView alwaysBounceVertical={false} style={{marginBottom:55}}>
         <View style={{padding:5,margin:15,marginBottom:10,borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
             <Text style={{textAlign:'center',fontSize:18,fontFamily:'Poppins_Medium'}}>{"Overview"}</Text>
             <Text style={{fontFamily:'Poppins_Medium',fontSize:15}}>Started On: {"22 August 2023, 11.09 PM"}</Text>
             <Text style={{fontFamily:'Poppins_Medium',fontSize:15}}>Status: {"Ongoing"}</Text>
             <Text style={{fontFamily:'Poppins_Medium',fontSize:15}}>Remaining: {"10 min, 23 Sec"}</Text>
         </View >
-        <View style={{flex:1/2,padding:2,margin:15,marginBottom:10,borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-            <View style={{flex:1,margin:2,flexDirection:'row',alignItems:'center'}}>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>01</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>02</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>03</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>04</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>05</Text>
-                </View>
-            </View>
-            <View style={{flex:1,margin:2,flexDirection:'row',alignItems:'center'}}>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>01</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>02</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>03</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>04</Text>
-                </View>
-                <View style={{flex:1,alignItems:'center',margin:2,height:'100%',justifyContent:'center',borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
-                    <Text>05</Text>
-                </View>
-            </View>
-        </View>
-        <View style={{flex:1,padding:5,margin:15,marginBottom:55,borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
+        <View style={{flexDirection:'row',flexWrap:'wrap',justifyContent:'space-between',padding:5,margin:15,marginBottom:10,borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >01</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >02</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >03</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >04</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >05</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >06</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >07</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >08</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >09</Text>
+            <Text style={{borderColor:'gray',borderWidth:1, borderRadius:5,minWidth:60,margin:2, maxWidth:'50', padding:8, textAlign:'center'}} >10</Text>
+        </View >
+        
+        <View style={{padding:5,margin:15,marginBottom:10,borderColor:'lightblue',borderWidth:1,borderRadius:10}}>
             <Text style={{fontFamily:'Poppins_Light',fontSize:16}}>{"01."} {"What does a circular road sign with a red border and a white center indicate?"}</Text>
-            <View style={{flex:1,alignItems:'flex-start'}}>
-                <RadioGroup 
-                containerStyle={{flex:1, alignItems:"flex-start"}}
+            <RadioGroup 
+                containerStyle={{alignItems:"flex-start"}}
                 radioButtons={radioButtons}
                 onPress={setSelectedId}
                 selectedId={selectedId}
                 />
-            </View>
         </View>
-        <View style={{flex:1, flexDirection:'row' }}>
-            <View style={{flex:1}}>
+        <View style={{ flexDirection:'row', justifyContent:'space-between' }}>
+            <View style={{flex:1,maxWidth:200}}>
                 <PrimaryButton >
                     <Text style={{fontFamily:'Poppins_Medium',fontSize:15}}>Previous</Text>
                 </PrimaryButton>
             </View>
-            <View style={{flex:1}}>
+            <View style={{flex:1, maxWidth:200}}>
                 <PrimaryButton  >
                     <Text style={{fontFamily:'Poppins_Medium',fontSize:15}}>Next</Text>
                 </PrimaryButton>
             </View>
-            
-        </View>
+        </View> 
+        </ScrollView>
     </View>
   )
 }
