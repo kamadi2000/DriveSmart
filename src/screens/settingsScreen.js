878023f6519ded40457d0879654f600377d7aacb
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-13 02:18:01 +0530

    settings updated

--- src/screens/settingsScreen.js
+++ src/screens/settingsScreen.js
@@ -1,11 +1,32 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { BACKGROUND_COLOR, PRIMARY_COLOR, RIPPLE_COLOR } from '../utils/colors'
-import Profile from '../assets/images/profile.png'
+
 import { Ionicons } from '@expo/vector-icons';
 import { ScrollView } from 'react-native';
-import { Pressable } from 'react-native';
+import { Pressable,Share } from 'react-native';
+import { Platform, ToastAndroid } from 'react-native';
 const SettingsScreen = () => {
+    const shareOurDownloadLink = async ()=>{
+        try{
+            const ourUrlPlayStore = 'https://play.google.com/store/apps/details?'
+            const ourUrlAppStore = 'https://apps.apple.com/app/your-app-name/'
+            if(Platform.OS == "ios"){
+                await Share.share({
+                    message:ourUrlAppStore
+                })
+            }
+            else{
+                await Share.share({
+                    title:'Share DriveSmart',
+                    message:ourUrlPlayStore
+                })
+            }
+        } catch(error){
+            ToastAndroid.show(`Failed to Share!`, ToastAndroid.SHORT);//only on android 
+            // console.log(error.message)
+        }
+    }
   return (
     <View style={{flex:1,backgroundColor:PRIMARY_COLOR,paddingTop:40}}>
         <View style={{alignItems:'center', padding:10}}>
@@ -15,20 +36,25 @@ const SettingsScreen = () => {
             <Text style={{fontFamily:'Poppins_Medium', fontSize:15}}>Google: Someone@gmail.com</Text>
         </View>
         <View style={{flex:1 ,overflow:'hidden',backgroundColor:BACKGROUND_COLOR,borderTopRightRadius:20, borderTopLeftRadius:20}}>
-            <ScrollView style={{paddingTop:20, padding:10}}>
+            <ScrollView 
                 
-                    <View style={{overflow:'hidden',borderRadius:15,margin:5}}>
-                        <Pressable style={{backgroundColor:'white',padding:15, flexDirection:'row', alignItems:'center'}} android_ripple={{color:'lightgray'}}>
-                            <Ionicons   name="person-outline" size={25} color="gray" />
-                            <Text style={{fontFamily:'Poppins_Regular', fontSize:16, paddingStart:15}}>Profile</Text>
-                        </Pressable>
-                    </View>
+                style={{paddingTop:20, padding:10}}>
+                
+                <View style={{overflow:'hidden',borderRadius:15,margin:5}}>
+                    <Pressable style={{backgroundColor:'white',padding:15, flexDirection:'row', alignItems:'center'}} android_ripple={{color:'lightgray'}}>
+                        <Ionicons   name="person-outline" size={25} color="gray" />
+                        <Text style={{fontFamily:'Poppins_Regular', fontSize:16, paddingStart:15}}>Profile</Text>
+                        <Ionicons style={{flex:1,textAlign:'right'}} name="chevron-forward" size={25} color="gray" />
+                    </Pressable>
+                </View>
                 
                 
                 <View style={{overflow:'hidden',borderRadius:15,margin:5}}>
                 <Pressable style={{backgroundColor:'white',padding:15, flexDirection:'row', alignItems:'center'}} android_ripple={{color:'lightgray'}}>
                     <Ionicons   name="language" size={25} color="gray" />
                     <Text style={{fontFamily:'Poppins_Regular', fontSize:16, paddingStart:15}}>Prefered Language</Text>
+                    <Text style={{flex:1,fontFamily:'Poppins_Regular',textAlign:'right',color:'gray', fontSize:14, paddingStart:15}}>English</Text>
+                    <Ionicons style={{textAlign:'right'}} name="chevron-forward" size={25} color="gray" />
                 </Pressable>
                 </View>
                 
@@ -51,7 +77,7 @@ const SettingsScreen = () => {
                 </Pressable>
                 </View>
                 <View style={{overflow:'hidden',borderRadius:15,margin:5}}>
-                <Pressable style={{backgroundColor:'white',padding:15, flexDirection:'row', alignItems:'center'}} android_ripple={{color:'lightgray'}}>
+                <Pressable onPress={shareOurDownloadLink} style={{backgroundColor:'white',padding:15, flexDirection:'row', alignItems:'center'}} android_ripple={{color:'lightgray'}}>
                     <Ionicons name="md-share-social-outline" size={30} color="gray" />
                     <Text style={{fontFamily:'Poppins_Regular', fontSize:16, paddingStart:15}}>Share</Text>
                 </Pressable>
@@ -62,6 +88,9 @@ const SettingsScreen = () => {
                     <Text style={{fontFamily:'Poppins_Regular', fontSize:16, paddingStart:15}}>Log out</Text>
                 </Pressable>
                 </View>
+
+
+                
                
             </ScrollView>
         </View>
