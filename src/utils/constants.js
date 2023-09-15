ec22374859ed86716dded3acc07daa5efebb3013
kamadi2000 <kmaliyanage@gmail.com>
2023-09-16 01:09:31 +0530

    constact updated with ngrok

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,4 +7,5 @@ export const WIDTH = Dimensions.get('window').width;
 export const SCREEN_HEIGHT = Dimensions.get('screen').height;
 export const CARD_COLOR = '#FF5733';
 
+//change with your own url when running
 export const BACKEND_URL = "https://ebcb-2402-4000-b280-146e-6cc4-cc31-3c5e-656f.ngrok.io/api"
